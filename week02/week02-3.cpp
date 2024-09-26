251e39d2e33044454d57126f9ac6345873e4d504
yeegii <144758476+yeegii@users.noreply.github.com>
2024-09-26 19:25:44 +0800

    week02

--- week02/week02-3.cpp
+++ week02/week02-3.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    string mergeAlternately(string word1, string word2) 
+    string mergeAlternately(string word1, string word2)
     {
         string ans; // 宣告一個字串 當答案
         for(int i=0;i<word1.length() || i<word2.length();i++)
         {
-            if(i<word1.length()) 
+            if(i<word1.length())
                 ans+=word1[i];
             if(i<word2.length())
                 ans+=word2[i];
