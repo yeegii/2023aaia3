61369b0668f232151d8583e409d8e5b2e7fec95d
yeegii <144758476+yeegii@users.noreply.github.com>
2024-09-26 20:10:54 +0800

    Delete week03-4b.py

--- week03-4b.py
+++
@@ -1,7 +0,0 @@
-///week03-4b.py leetcode 28. Find the Index of the First Occurrence in a String
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        a = haystack
-        b = needle
-
-        return a.find(b)
