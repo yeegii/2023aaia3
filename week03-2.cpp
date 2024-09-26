7cb838cc9a56b71308744c0eba2be48b542cedfb
yeegii <144758476+yeegii@users.noreply.github.com>
2024-09-26 20:09:02 +0800

    Delete week03-2.cpp

--- week03-2.cpp
+++
@@ -1,9 +0,0 @@
-///week03-2.cpp 用c語言寫
-#include <stdio.h>
-int main()
-{
-    printf("%d\n",'a');///單引號 單1個字母
-    printf("%d\n","a");///雙引號 很多字母 字串 陣列
-    if('a'==97)
-            printf("原來a的值,就是97\n");
-}
