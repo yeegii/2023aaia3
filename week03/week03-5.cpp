2d6fbf09afb56deec59e9df01b54f80e7364043d
yeegii <144758476+yeegii@users.noreply.github.com>
2024-09-26 20:07:46 +0800

    Delete week03 directory

--- week03/week03-5.cpp
+++
@@ -1,9 +0,0 @@
-///week03-5.cpp
-class Solution {
-public:
-    bool repeatedSubstringPattern(string s) {
-        string ans = s + s;
-        string sub= ans.substr(1, ans.size()-2);
-        return sub.find(s) != string::npos;
-    }
-};
