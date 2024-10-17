a976bac5596e739a4bc5fbe50374ef59300afd11
yeegii <144758476+yeegii@users.noreply.github.com>
2024-10-17 16:39:04 +0800

    week06

---
+++ week06/week06-1.cpp
@@ -0,0 +1,20 @@
+//week06-1.cpp
+# include <iostream>
+# include <vector>
+# include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> a;
+    a.push_back(20);
+    a.push_back(30);
+    a.push_back(50);
+    a.push_back(10);
+    sort(a.begin(),a.end());
+    for(int i=0;i<a.size();i++)
+    {
+        cout << a[i] << endl;
+    }
+
+
+}
---
+++ week06/week06-2a.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <vector>
+# include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int>a;
+    int n;
+    while(cin >> n)
+    {
+        a.push_back(n);
+        cout << endl;
+
+
+    }
+}
