8d706ed5540bd3560fc2156312a49ec33025fcf9
yeegii <144758476+yeegii@users.noreply.github.com>
2024-12-12 16:09:17 +0800

    week14

---
+++ week14/week14-2.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans=new ListNode();
+        int carry=0;
+        ListNode * now = ans; //現在要處理得NODE
+        while(l1 != nullptr || l2 != nullptr)
+        {
+            if(l1==nullptr)
+            {
+                int here = l2->val +carry;
+                carry=here/10;
+                now->next = new ListNode( here%10 );
+                now = now->next;
+                l2 = l2->next;
+            }
+            else if(l2==nullptr)
+            {
+                int here = l1->val +carry;
+                carry=here/10;
+                now->next = new ListNode( here%10 );
+                now = now->next;
+                l1 = l1->next;
+            }
+            else
+            {
+                int here = l1->val + l2->val +carry;
+                carry=here/10;
+                now->next = new ListNode( here%10 );
+                now = now->next;
+                l1 = l1->next;
+                l2 = l2->next;
+            }
+            if()
+        }
+        return ans->next;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
---
+++ week14/week14-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	while(cin >> a >> b)
+	{
+		while(a>0)
+		{
+			cout << a << " ";
+			a/=b;
+		}
+
+		cout << "Boring!" << endl;
+	}
+}
